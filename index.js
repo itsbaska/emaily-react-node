452a54dfdc34a69a1be514e7b92f464c2c1e16d2
Baska Batbold <baskabatbold@gmail.com>
2018-02-11 00:00:02 -0600

    add passport route

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
